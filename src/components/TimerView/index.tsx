318fe61910f09782454c69fa733df682dbc866a9
popomon <popomonclub@gmail.com>
2023-03-06 23:07:02 +0900

    [WIP]: Added timer view interface and sample data

--- src/components/TimerView/index.tsx
+++ src/components/TimerView/index.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import useTimer from '@/hooks/useTimer';
+import React, {useEffect} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 
-interface Todo {
+interface TimerItem {
   id: string;
   title: string;
   duration: number;
 }
 
-const todoList: Todo[] = [
-  {id: '1', title: 'Todo 1', duration: 10},
-  {id: '2', title: 'Todo 2', duration: 20},
-  {id: '3', title: 'Todo 3', duration: 30},
-  {id: '4', title: 'Todo 4', duration: 40},
-  {id: '5', title: 'Todo 5', duration: 50},
+const timerItems: TimerItem[] = [
+  {id: '1', title: 'TimerItem 1', duration: 10},
+  {id: '2', title: 'TimerItem 2', duration: 20},
+  {id: '3', title: 'TimerItem 3', duration: 30},
+  {id: '4', title: 'TimerItem 4', duration: 40},
+  {id: '5', title: 'TimerItem 5', duration: 50},
 ];
 
 interface Props {}
 
 const TimerView = ({}: Props) => {
-  console.log(todoList);
+  const {timeInSeconds, start, stop, pause, restart} = useTimer({});
+
+  useEffect(() => {
+    start();
+  }, [start]);
+
+  const handlePause = () => {
+    pause();
+  };
+
+  const handleRestart = () => {
+    restart();
+  };
 
   return (
     <View>
-      <Text>123</Text>
+      <Text>{timeInSeconds}</Text>
+      <TouchableOpacity onPress={handlePause}>
+        <Text>pause</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleRestart}>
+        <Text>restart</Text>
+      </TouchableOpacity>
     </View>
   );
 };
