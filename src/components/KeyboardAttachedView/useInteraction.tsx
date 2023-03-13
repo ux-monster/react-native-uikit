d29aa57c91dca458fc91cd4fb57b5e7d58be2f12
Jisung Hong <88924462+popocreator@users.noreply.github.com>
2023-03-14 00:38:58 +0900

    fix: Fixed animated style value - visible,invisible (#2)

--- src/components/KeyboardAttachedView/useInteraction.tsx
+++ src/components/KeyboardAttachedView/useInteraction.tsx
@@ -1,13 +1,22 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Keyboard, Platform, KeyboardEvent, TextInput} from 'react-native';
-import {useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+import {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 const useInteraction = () => {
   const [visibleKeyboard, setVisibleKeyboard] = useState<boolean>(false);
   const keyboardHeight = useSharedValue<number>(0);
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const bottomContainerHeight = useRef<number>(0);
-  const bottomContainerTransitionHeight = useSharedValue<number>(0);
+  const bottomContainerHeight = useSharedValue<number>(0);
+  const bottomContainerViewStyle = useAnimatedStyle(() => {
+    return {
+      height: bottomContainerHeight.value,
+    };
+  });
 
   const handleShowKeyboard = useCallback(
     (e: KeyboardEvent) => {
@@ -56,9 +65,15 @@ const useInteraction = () => {
   const showingAddOn = useRef<boolean>(false);
   const textInputRef = useRef<TextInput>(null);
 
+  const invisibleAddOn = () => {
+    setVisibleAddOn(false);
+  };
+
   const handleBlur = () => {
     if (!showingAddOn.current) {
-      setVisibleAddOn(false);
+      bottomContainerHeight.value = withTiming(0, {}, () => {
+        runOnJS(invisibleAddOn)();
+      });
     }
   };
 
@@ -83,7 +98,7 @@ const useInteraction = () => {
     textInputRef,
     selectedIndex,
     bottomContainerHeight,
-    bottomContainerTransitionHeight,
+    bottomContainerViewStyle,
     visibleAddOn,
     visibleKeyboard,
     handleBlur,
