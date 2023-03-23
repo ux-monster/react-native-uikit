12a1a6b9476eda28c12e6bf1de3938e2ffbea102
popomon <popomonclub@gmail.com>
2023-03-23 23:48:34 +0900

    fix: Activated tab, stack navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import StackNavigator, {StackParams} from './_stackNavigator';
-// import TabNavigator, {TabParams} from './_tabNavigator';
+import TabNavigator, {TabParams} from './tabNavigator';
+import StackNavigator, {StackParams} from './stackNavigator';
 
 export type RootStackParamList = {
   Hello1: undefined;
   Hello2: undefined;
   Hello3: undefined;
-  // Stack: StackParams;
-  // Tab: TabParams;
+  Stack: StackParams;
+  Tab: TabParams;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -19,8 +19,8 @@ const RootNavigation = () => {
       <Stack.Screen name="Hello1" component={() => <></>} />
       <Stack.Screen name="Hello2" component={() => <></>} />
       <Stack.Screen name="Hello3" component={() => <></>} />
-      {/* <Stack.Screen name="Stack" component={StackNavigator} /> */}
-      {/* <Stack.Screen name="Tab" component={TabNavigator} /> */}
+      <Stack.Screen name="Stack" component={StackNavigator} />
+      <Stack.Screen name="Tab" component={TabNavigator} />
     </Stack.Navigator>
   );
 };
